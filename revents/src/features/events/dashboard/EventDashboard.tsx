b9fc3add46523c79a4318d19557d40a25f0972b9
MWardill <mwardill@pdisoftware.com>
2025-08-09 18:44:47 +0100

    Lesson 22

--- revents/src/features/events/dashboard/EventDashboard.tsx
+++ revents/src/features/events/dashboard/EventDashboard.tsx
@@ -3,6 +3,7 @@ import { events } from "../../../lib/data/sampleData";
 import type { AppEvent } from "../../../lib/types";
 import EventForm from "../form/EventForm";
 import EventCard from "./EventCard";
+import { AnimatePresence, motion } from "motion/react";
 
 
 type Props = {
@@ -27,17 +28,44 @@ export default function EventDashboard({ formOpen, setFormOpen }: Props) {
   return (
     <div className="flex flex-row w-full gap-6">
 
-      <div className="w-3/5 flex flex-col gap-4">
-        {appEvents.map((event) => (
-          <EventCard key={event.id} event={event} />
-        ))}
+      <div className="w-3/5">
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -200 }}
+            transition={{ ease: "easeInOut", duration: 0.7 }}
+          >
+            <div className="flex flex-col gap-4">
+              {appEvents.map((event) => (
+                <EventCard key={event.id} event={event} />
+              ))}
+
+              /* Just load two for now */
+              {appEvents.slice(0, 2).map((event) => (
+                <EventCard key={event.id} event={event} />
+              ))}
+
+            </div>
+
+          </motion.div>
+        </AnimatePresence>
+
       </div>
 
       <div className="w-2/5">
         <div className="sticky top-20">
-          {formOpen && (
-            <EventForm setFormOpen={setFormOpen} />
-          )}          
+          <AnimatePresence>
+            {formOpen && (
+              <motion.div
+                initial={{ opacity: 0, x: 200 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 200 }}
+                transition={{ ease: "easeInOut", duration: 0.5 }}>
+                <EventForm setFormOpen={setFormOpen} />
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
 
