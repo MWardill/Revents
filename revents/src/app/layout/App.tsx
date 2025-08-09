a98af38ebf2bc4c2e3c4cddc66e6ff5c2cf0e640
MWardill <mwardill@pdisoftware.com>
2025-08-09 16:35:49 +0100

    lesson 14

--- revents/src/app/layout/App.tsx
+++ revents/src/app/layout/App.tsx
@@ -1,10 +1,13 @@
+import EventDashboard from "../../features/events/dashboard/EventDashboard"
+
 function App() {
 
   return (
     <>
       <div className="p-5">
-        <h1 className="text-4xl text-red-500">Revents app</h1>
+        <h1 className="text-4xl text-red-500 font-heading">Revents app</h1>
         <button type="button" className="btn btn-primary">Click Me</button>
+        <EventDashboard />
       </div>            
     </>
   )
---
+++ revents/src/features/events/dashboard/EventDashboard.tsx
@@ -0,0 +1,11 @@
+export default function EventDashboard() {
+
+  return (
+    <div className="flex flex-row w-full">
+      <div className="w-3/5">Left Content</div>
+      <div className="w-2/5">Right Content</div>
+    </div>
+  )
+
+
+}
